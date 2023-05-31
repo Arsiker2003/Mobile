005e3a69884fb15a15a510bd9c05687e6373ee9f
Arsiker2003 <davydenko2003ar@gmail.com>
2023-05-31 12:47:56 +0300

    RestartAdded

--- Lab5/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ Lab5/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -15,12 +15,12 @@ class GameViewModel : ViewModel() {
             getNextWord()
         } else {
             _currentScrambledWord = String(tempWord)
-            ++currentWordCount
+            ++_currentWordCount
             wordsList.add(currentWord)
         }
     }
     fun nextWord(): Boolean {
-        return if (currentWordCount < MAX_NO_OF_WORDS) {
+        return if (_currentWordCount < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
@@ -29,7 +29,9 @@ class GameViewModel : ViewModel() {
     private var _score = 0
     val score: Int
         get() = _score
-    private var currentWordCount = 0
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
     private lateinit var _currentScrambledWord : String
     val currentScrambledWord: String
         get() = _currentScrambledWord
@@ -53,5 +55,11 @@ class GameViewModel : ViewModel() {
             return true
         }
         return false
+    }
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
     }
 }
